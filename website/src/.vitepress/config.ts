tachiyomiorg/website

Fix remaining eslint errors

website/src/.vitepress/config.ts
@@ -1,72 +1,76 @@
-import process from "node:process"
-import { URL, fileURLToPath } from "node:url"
-import { defineConfig, loadEnv } from "vitepress"
-import ElementPlus from "unplugin-element-plus/vite"
+import process from 'node:process'
+import { URL, fileURLToPath } from 'node:url'
+import { defineConfig, loadEnv } from 'vitepress'
+import ElementPlus from 'unplugin-element-plus/vite'
 
-import markdownConfig from "./config/markdownConfig"
+import markdownConfig from './config/markdownConfig'
 
 // For use with loading Markdown plugins
-import themeConfig from "./config/themeConfig"
+import themeConfig from './config/themeConfig'
 
 // Theme related config
-import headConfig from "./config/headConfig" // Provides how to generate Meta head tags
+import headConfig from './config/headConfig'
 
-import generateMeta from "./config/hooks/generateMeta"
+// Provides how to generate Meta head tags
+
+import generateMeta from './config/hooks/generateMeta'
 
 // Enhanced meta generation
-import generateFeed from "./config/hooks/generateFeed" // Allows generation of RSS feed
-import generateOgImages from "./config/hooks/generateOgImages"
+import generateFeed from './config/hooks/generateFeed'
 
-const title = "Tachiyomi"
-const description = "Discover and read manga, webtoons, comics, and more – easier than ever on your Android device."
+// Allows generation of RSS feed
+import generateOgImages from './config/hooks/generateOgImages'
 
-const env = loadEnv("", process.cwd())
-const hostname: string = env.VITE_HOSTNAME || "http://localhost:4173"
+const title = 'Tachiyomi'
+const description = 'Discover and read manga, webtoons, comics, and more – easier than ever on your Android device.'
+
+const env = loadEnv('', process.cwd())
+const hostname: string = env.VITE_HOSTNAME || 'http://localhost:4173'
 
 export default defineConfig({
-	outDir: "../dist",
-	lastUpdated: true,
-	cleanUrls: true,
-	title,
-	description,
-	sitemap: {
-		hostname,
-	},
-	head: headConfig,
-	markdown: markdownConfig,
-	themeConfig,
-	transformHead: async (context) => generateMeta(context, hostname),
-	buildEnd: async (context) => {
-		generateFeed(context, hostname)
-		generateOgImages(context)
-	},
-	vite: {
-		resolve: {
-			alias: [
-				{
-					// Used to show the release version on navbar.
-					find: /^.*\/VPNavBarMenu\.vue$/,
-					replacement: fileURLToPath(
-						new URL("./theme/components/CustomNavBarMenu.vue", import.meta.url),
-					),
-				},
-				{
-					find: /^.*VPNavScreenMenu\.vue$/,
-					replacement: fileURLToPath(
-						new URL("./theme/components/CustomNavScreenMenu.vue", import.meta.url),
-					),
-				},
-				{
-					find: /^.*VPSwitchAppearance\.vue$/,
-					replacement: fileURLToPath(
-						new URL("./theme/components/CustomSwitchAppearance.vue", import.meta.url),
-					),
-				},
-			],
-		},
-		plugins: [ElementPlus({})],
-		ssr: {
-			noExternal: ["element-plus"],
-		},
-	},
+  outDir: '../dist',
+  lastUpdated: true,
+  cleanUrls: true,
+  title,
+  description,
+  sitemap: {
+    hostname,
+  },
+  head: headConfig,
+  markdown: markdownConfig,
+  themeConfig,
+  transformHead: async context => generateMeta(context, hostname),
+  buildEnd: async (context) => {
+    generateFeed(context, hostname)
+    generateOgImages(context)
+  },
+  vite: {
+    resolve: {
+      alias: [
+        {
+          // Used to show the release version on navbar.
+          find: /^.*\/VPNavBarMenu\.vue$/,
+          replacement: fileURLToPath(
+            new URL('./theme/components/CustomNavBarMenu.vue', import.meta.url),
+          ),
+        },
+        {
+          find: /^.*VPNavScreenMenu\.vue$/,
+          replacement: fileURLToPath(
+            new URL('./theme/components/CustomNavScreenMenu.vue', import.meta.url),
+          ),
+        },
+        {
+          find: /^.*VPSwitchAppearance\.vue$/,
+          replacement: fileURLToPath(
+            new URL('./theme/components/CustomSwitchAppearance.vue', import.meta.url),
+          ),
+        },
+      ],
+    },
+    plugins: [ElementPlus({})],
+    ssr: {
+      noExternal: ['element-plus'],
+    },
+  },
 })

website/src/.vitepress/config/headConfig.ts
@@ -1,59 +1,59 @@
-import type { HeadConfig } from "vitepress"
+import type { HeadConfig } from 'vitepress'
 
 const headConfig: HeadConfig[] = [
-	["meta", { name: "darkreader-lock" }],
+  ['meta', { name: 'darkreader-lock' }],
 
-	["meta", { name: "theme-color", content: "#818CF8" }],
-	["meta", { name: "msapplication-TileColor", content: "#818CF8" }],
+  ['meta', { name: 'theme-color', content: '#818CF8' }],
+  ['meta', { name: 'msapplication-TileColor', content: '#818CF8' }],
 
-	["meta", { name: "viewport", content: "width=device-width, initial-scale=1.0" }],
-	["meta", { name: "referrer", content: "no-referrer-when-downgrade" }],
+  ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }],
+  ['meta', { name: 'referrer', content: 'no-referrer-when-downgrade' }],
 
-	["link", { rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
-	[
-		"link",
-		{
-			rel: "icon",
-			type: "image/png",
-			sizes: "32x32",
-			href: "/favicon-32x32.png",
-		},
-	],
-	[
-		"link",
-		{
-			rel: "icon",
-			type: "image/png",
-			sizes: "16x16",
-			href: "/favicon-16x16.png",
-		},
-	],
-	["link", { rel: "manifest", href: "/site.webmanifest" }],
-	["link", { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#818CF8" }],
-	[
-		"link",
-		{
-			rel: "apple-touch-icon",
-			type: "image/x-icon",
-			sizes: "180x180",
-			href: "/favicon.ico",
-		},
-	],
+  ['link', { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  [
+    'link',
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      href: '/favicon-32x32.png',
+    },
+  ],
+  [
+    'link',
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      href: '/favicon-16x16.png',
+    },
+  ],
+  ['link', { rel: 'manifest', href: '/site.webmanifest' }],
+  ['link', { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#818CF8' }],
+  [
+    'link',
+    {
+      rel: 'apple-touch-icon',
+      type: 'image/x-icon',
+      sizes: '180x180',
+      href: '/favicon.ico',
+    },
+  ],
 
-	["meta", { name: "twitter:card", content: "summary" }],
-	["meta", { name: "twitter:site", content: "@tachiyomiorg" }],
-	["meta", { name: "twitter:creator", content: "@tachiyomiorg" }],
+  ['meta', { name: 'twitter:card', content: 'summary' }],
+  ['meta', { name: 'twitter:site', content: '@tachiyomiorg' }],
+  ['meta', { name: 'twitter:creator', content: '@tachiyomiorg' }],
 
-	["meta", { property: "og:site_name", content: "Tachiyomi" }],
-	[
-		"meta",
-		{
-			property: "og:description",
-			content: "Discover and read manga, webtoons, comics, and more – easier than ever on your Android device.",
-		},
-	],
-	["meta", { property: "og:locale", content: "en_US" }],
-	["meta", { property: "og:type", content: "website" }],
+  ['meta', { property: 'og:site_name', content: 'Tachiyomi' }],
+  [
+    'meta',
+    {
+      property: 'og:description',
+      content: 'Discover and read manga, webtoons, comics, and more – easier than ever on your Android device.',
+    },
+  ],
+  ['meta', { property: 'og:locale', content: 'en_US' }],
+  ['meta', { property: 'og:type', content: 'website' }],
 ]
 
 export default headConfig

aniyomiorg/aniyomi-website

Co-authored-by: arkon <4098258+arkon@users.noreply.github.com>

website/src/.vitepress/config.ts
@@ -1,72 +1,74 @@
-import process from "node:process"
-import { URL, fileURLToPath } from "node:url"
-import { defineConfig, loadEnv } from "vitepress"
-import ElementPlus from "unplugin-element-plus/vite"
+import process from 'node:process'
+import { URL, fileURLToPath } from 'node:url'
+import { defineConfig, loadEnv } from 'vitepress'
+import ElementPlus from 'unplugin-element-plus/vite'
 
-import markdownConfig from "./config/markdownConfig"
+import markdownConfig from './config/markdownConfig'
 
 // For use with loading Markdown plugins
-import themeConfig from "./config/themeConfig"
+import themeConfig from './config/themeConfig'
 
 // Theme related config
-import headConfig from "./config/headConfig" // Provides how to generate Meta head tags
+import headConfig from './config/headConfig'
 
-import generateMeta from "./config/hooks/generateMeta"
+import generateMeta from './config/hooks/generateMeta'
 
 // Enhanced meta generation
-import generateFeed from "./config/hooks/generateFeed" // Allows generation of RSS feed
-import generateOgImages from "./config/hooks/generateOgImages"
+import generateFeed from './config/hooks/generateFeed'
 
-const title = "Aniyomi"
-const description = "Discover and watch anime, cartoons, series, and more – easier than ever on your Android device."
+// Allows generation of RSS feed
+import generateOgImages from './config/hooks/generateOgImages'
 
-const env = loadEnv("", process.cwd())
-const hostname: string = env.VITE_HOSTNAME || "http://localhost:4173"
+const title = 'Aniyomi'
+const description = 'Discover and watch anime, cartoons, series, and more – easier than ever on your Android device.'
+
+const env = loadEnv('', process.cwd())
+const hostname: string = env.VITE_HOSTNAME || 'http://localhost:4173'
 
 export default defineConfig({
-	outDir: "../dist",
-	lastUpdated: true,
-	cleanUrls: true,
-	title,
-	description,
-	sitemap: {
-		hostname,
-	},
-	head: headConfig,
-	markdown: markdownConfig,
-	themeConfig,
-	transformHead: async (context) => generateMeta(context, hostname),
-	buildEnd: async (context) => {
-		generateFeed(context, hostname)
-		generateOgImages(context)
-	},
-	vite: {
-		resolve: {
-			alias: [
-				{
-					// Used to show the release version on navbar.
-					find: /^.*\/VPNavBarMenu\.vue$/,
-					replacement: fileURLToPath(
-						new URL("./theme/components/CustomNavBarMenu.vue", import.meta.url),
-					),
-				},
-				{
-					find: /^.*VPNavScreenMenu\.vue$/,
-					replacement: fileURLToPath(
-						new URL("./theme/components/CustomNavScreenMenu.vue", import.meta.url),
-					),
-				},
-				{
-					find: /^.*VPSwitchAppearance\.vue$/,
-					replacement: fileURLToPath(
-						new URL("./theme/components/CustomSwitchAppearance.vue", import.meta.url),
-					),
-				},
-			],
-		},
-		plugins: [ElementPlus({})],
-		ssr: {
-			noExternal: ["element-plus"],
-		},
-	},
+  outDir: '../dist',
+  lastUpdated: true,
+  cleanUrls: true,
+  title,
+  description,
+  sitemap: {
+    hostname,
+  },
+  head: headConfig,
+  markdown: markdownConfig,
+  themeConfig,
+  transformHead: async (context) => generateMeta(context, hostname),
+  buildEnd: async (context) => {
+    generateFeed(context, hostname)
+    generateOgImages(context)
+  },
+  vite: {
+    resolve: {
+      alias: [
+        {
+          // Used to show the release version on navbar.
+          find: /^.*\/VPNavBarMenu\.vue$/,
+          replacement: fileURLToPath(
+            new URL('./theme/components/CustomNavBarMenu.vue', import.meta.url),
+          ),
+        },
+        {
+          find: /^.*VPNavScreenMenu\.vue$/,
+          replacement: fileURLToPath(
+            new URL('./theme/components/CustomNavScreenMenu.vue', import.meta.url),
+          ),
+        },
+        {
+          find: /^.*VPSwitchAppearance\.vue$/,
+          replacement: fileURLToPath(
+            new URL('./theme/components/CustomSwitchAppearance.vue', import.meta.url),
+          ),
+        },
+      ],
+    },
+    plugins: [ElementPlus({})],
+    ssr: {
+      noExternal: ['element-plus'],
+    },
+  },
 })

website/src/.vitepress/config/headConfig.ts
@@ -1,55 +1,55 @@
-import type { HeadConfig } from "vitepress"
+import type { HeadConfig } from 'vitepress'
 
 const headConfig: HeadConfig[] = [
-	["meta", { name: "darkreader-lock" }],
+  ['meta', { name: 'darkreader-lock' }],
 
-	["meta", { name: "theme-color", content: "#818CF8" }],
-	["meta", { name: "msapplication-TileColor", content: "#818CF8" }],
+  ['meta', { name: 'theme-color', content: '#818CF8' }],
+  ['meta', { name: 'msapplication-TileColor', content: '#818CF8' }],
 
-	["meta", { name: "viewport", content: "width=device-width, initial-scale=1.0" }],
-	["meta", { name: "referrer", content: "no-referrer-when-downgrade" }],
+  ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }],
+  ['meta', { name: 'referrer', content: 'no-referrer-when-downgrade' }],
 
-	["link", { rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
-	[
-		"link",
-		{
-			rel: "icon",
-			type: "image/png",
-			sizes: "32x32",
-			href: "/favicon-32x32.png",
-		},
-	],
-	[
-		"link",
-		{
-			rel: "icon",
-			type: "image/png",
-			sizes: "16x16",
-			href: "/favicon-16x16.png",
-		},
-	],
-	["link", { rel: "manifest", href: "/site.webmanifest" }],
-	["link", { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#818CF8" }],
-	[
-		"link",
-		{
-			rel: "apple-touch-icon",
-			type: "image/x-icon",
-			sizes: "180x180",
-			href: "/favicon.ico",
-		},
-	],
+  ['link', { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  [
+    'link',
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      href: '/favicon-32x32.png',
+    },
+  ],
+  [
+    'link',
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      href: '/favicon-16x16.png',
+    },
+  ],
+  ['link', { rel: 'manifest', href: '/site.webmanifest' }],
+  ['link', { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#818CF8' }],
+  [
+    'link',
+    {
+      rel: 'apple-touch-icon',
+      type: 'image/x-icon',
+      sizes: '180x180',
+      href: '/favicon.ico',
+    },
+  ],
 
-	["meta", { property: "og:site_name", content: "Aniyomi" }],
-	[
-		"meta",
-		{
-			property: "og:description",
-			content: "Discover and watch anime, cartoons, series, and more – easier than ever on your Android device.",
-		},
-	],
-	["meta", { property: "og:locale", content: "en_US" }],
-	["meta", { property: "og:type", content: "website" }],
+  ['meta', { property: 'og:site_name', content: 'Aniyomi' }],
+  [
+    'meta',
+    {
+      property: 'og:description',
+      content: 'Discover and watch anime, cartoons, series, and more – easier than ever on your Android device.',
+    },
+  ],
+  ['meta', { property: 'og:locale', content: 'en_US' }],
+  ['meta', { property: 'og:type', content: 'website' }],
 ]
 
 export default headConfig